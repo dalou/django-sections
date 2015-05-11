0aba26f5a52134207d63cd4e7fae485180acea58
Dalou <autrusseau.damien@gmail.com>
2015-05-11 12:00:04 +0200

    FIX

--- sections/static/sections/js/plugins/char.js
+++ sections/static/sections/js/plugins/char.js
@@ -1,12 +1,38 @@
-sections_plugins['char'] = function($iframe, $field, options) {
+sections_plugins['text'] = function($iframe, $field, options) {
+    $iframe[0].data[options.name] = $field.html();
+    $field.dblclick(function($modal, id) {
+        id = (new Date().getTime() + Math.random()).toString().replace('.','_');
+        $modal = $('<div class="app app-modal">\
+            <div class="modal-head">Saissisez le contenu</div>\
+            <div class="modal-content-old"><textarea></textarea></div>\
+            <div class="modal-foot"><a class="btn btn-info pull-right">Valider</a></div>\
+        </div>');
+        $modal.find('textarea')
+            //.hide()
+            .attr('id', id)
+            .val($iframe[0].data[$field.data('form').name]);
+        $modal.find('a').click(function() {
+            var html = tinymce.activeEditor.getContent({format : 'raw'});
+            $iframe[0].data[$field.data('form').name] = html
+            $field.html(html);
+            console.log(content)
+            $modal.modal('close');
+        })
 
-    $iframe[0].data[options.name] = $.trim($field.text());
-    $field.dblclick(function() {
-        text = prompt('Saississez un texte', $iframe[0].data[options.name]);
-        if(text != null) {
-            $iframe[0].data[$field.data('form').name] = text;
-            $field.text(text);
-        }
+        $modal.modal('show');
+        tinymce.init({
+            selector: '#'+id,
+
+            plugins: [
+                "advlist lists charmap print preview ",
+                "searchreplace visualblocks",
+                "insertdatetime contextmenu paste"
+            ],
+            toolbar: "insertfile undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent "
+
+        });
         return false;
+
+
     })
 }
