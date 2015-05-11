98c3c7f0950187134971e3ced84f858ab5342dc1
Dalou <autrusseau.damien@gmail.com>
2015-05-11 10:10:20 +0200

    FIX

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-sections",
+    version = "1.0",
+    url='http://github.com/dalou/django-cargo',
+    license='BSD',
+    platforms=['OS Independent'],
+    description="Sections content managment",
+    setup_requires = [
+
+    ],
+    install_requires = [
+    ],
+    long_description=open('README.md').read(),
+    author='Damien Autrusseau',
+    author_email='autrusseau.damien@gmail.com',
+    maintainer='Damien Autrusseau',
+    maintainer_email='autrusseau.damien@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
